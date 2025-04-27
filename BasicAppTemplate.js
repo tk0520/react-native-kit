0a480e64d6689dc92851742262445d0cd024b747
solo <solo@localhost.localdomain>
2025-04-27 17:47:23 +0900

    BasicAppTemplate.js: created

---
+++ BasicAppTemplate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+function App() {
+  return (
+    <></>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default App;
