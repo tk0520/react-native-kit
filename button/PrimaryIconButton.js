fcec34f466f1bc1958443b359a5c950e1a076726
solo <gtj9966@gmail.com>
2025-07-27 21:26:25 +0900

    button/PrimaryIconButton.js: created. IconButton.js deleted.

---
+++ button/PrimaryIconButton.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet, View, Text, Pressable, Image } from "react-native";
+
+function PrimaryIconButton({title = "타이틀", icon, onPress, hasMarginBottom, marginBottomSize = 16}) {
+    return (
+      <View style={[styles.block, hasMarginBottom && { marginBottom: marginBottomSize }]}>
+        <Pressable
+          style={styles.wrapper}
+          android_ripple={{color: "ffffff", borderless: false}}
+          onPress={onPress}
+        >
+          <Image 
+            style={styles.icon}
+            source={icon}
+          />
+          <Text style={[styles.text.button]}>{title}</Text>
+        </Pressable>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    borderRadius: 20,   
+    overflow: "hidden", 
+    marginHorizontal: 5,       
+  },
+  wrapper: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#87A1FE",
+    paddingHorizontal: 20,
+    gap: 8,
+  },
+  text: {
+    button: {
+      fontSize: 16,   
+      color: "white"
+    },
+  },
+  icon: {
+    height: 24,
+    width: 24
+  }
+})
+
+export default PrimaryIconButton;
